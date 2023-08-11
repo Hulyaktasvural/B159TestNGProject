392a1d16d1d286df620643d660a91af4af3a729b
Hülya Vural <hulyaktas17@hotmail.com>
2023-08-11 15:24:19 +0300

    Initial commit

---
+++ src/test/java/techproed/tests/day23_Annotation/C01_Annotation.java
@@ -0,0 +1,54 @@
+package techproed.tests.day23_Annotation;
+
+import org.testng.annotations.*;
+
+public class C01_Annotation {
+    @BeforeSuite
+    public void suite(){
+        System.out.println("Herşeyden önce bir kez BeforeSuite çalışır");
+    }
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("Her testten önce bir kez çalışır");
+    }
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("Her classtan önce bir kez çalışır");
+    }
+    @BeforeMethod
+    public void setUp(){
+        System.out.println("Junit'teki @Before notasyonu gibi her methoddan önce çalışır");
+    }
+
+    @Test
+    public void test01() {
+        System.out.println("Test1 çalıştı");
+    }
+
+    @Test
+    public void test02() {
+        System.out.println("Test2 çalıştı");
+    }
+
+    @Test
+    public void test03() {
+        System.out.println("Test3 çalıştı");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("Her şeyden sonra 1 kez çalışır");
+    }
+    @AfterTest
+    public void afterTest(){
+        System.out.println("Her testten sonra 1 kez çalışır");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("Her class'dan sonra 1 kez çalışır");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.out.println("Junit'teki @After notasyonu gibi her methoddan sonra çalışır");
+    }
+}
